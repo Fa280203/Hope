23be66cd79f0f4eb9e959638a2fe0890a62109f7
fatou <ndeye.samba@efrei.net>
2024-12-05 16:46:11 +0100

    init

---
+++ src/main/java/com/example/hopeproject/HopeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hopeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopeProjectApplication.class, args);
+    }
+
+}
